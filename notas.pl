55103263e94b867b263cec163339667cd1a31404
César Gómez <cgoteng@hotmail.com>
2020-04-26 00:07:50 -0500

    Update notas.pl

--- notas.pl
+++ notas.pl
@@ -1,32 +1,34 @@
 % Estudiantes
-estudiante(juan).
-estudiante(carlos).
 estudiante(andrea).
-estudiate(cesar).
+estudiante(carlos).
+estudiante(cesar).
+estudiante(juan).
 
 % Cursos
 curso(informatica).
 curso(programacion).
 
 % Relacion estudiantes - curso
-es_estudiante(juan, informatica).
-es_estudiante(carlos, informatica).
-es_estudiante(andrea, informatica).
+estudiante(andrea, informatica).
+estudiante(carlos, informatica).
+estudiante(juan, informatica).
 
-es_estudiante(juan, programacion).
-es_estudiante(carlos, programacion).
-es_estudiante(andrea, programacion).
-es_estudiante(cesar, programacion).
+estudiante(andrea, programacion).
+estudiante(carlos, programacion).
+estudiante(cesar, programacion).
+estudiante(juan, programacion).
+
+% Regla para sumar los valores del array
+suma([],0).
+suma([N|Y],M):- suma(Y, M1), M is M1 + N.
 
 % Calificacion por estudiante
 calificacion(juan, informatica, 1.0, 0.3).
 calificacion(juan, informatica, 1.0, 0.3).
 calificacion(juan, informatica, 1.0, 0.4).
-
 calificacion(carlos, informatica, 1.0, 0.3).
 calificacion(carlos, informatica, 1.0, 0.3).
 calificacion(carlos, informatica, 1.0, 0.4).
-
 calificacion(andrea, informatica, 1.0, 0.3).
 calificacion(andrea, informatica, 1.0, 0.3).
 calificacion(andrea, informatica, 1.0, 0.4).
@@ -34,30 +36,21 @@ calificacion(andrea, informatica, 1.0, 0.4).
 calificacion(juan, programacion, 1.0, 0.3).
 calificacion(juan, programacion, 1.0, 0.3).
 calificacion(juan, programacion, 1.0, 0.4).
-
 calificacion(carlos, programacion, 1.0, 0.3).
 calificacion(carlos, programacion, 1.0, 0.3).
 calificacion(carlos, programacion, 1.0, 0.4).
-
 calificacion(andrea, programacion, 1.0, 0.3).
 calificacion(andrea, programacion, 1.0, 0.3).
 calificacion(andrea, programacion, 1.0, 0.4).
-
-% Fallas
-fallas(juan, programacion).
-fallas(juan, programacion).
-fallas(carlos, programacion).
+calificacion(cesar, programacion, 1.0, 0.3).
+calificacion(cesar, programacion, 1.0, 0.3).
+calificacion(cesar, programacion, 1.0, 0.4).
 
 % Producto de la nota y porcentaje
 nota_ponderada(E, C, R) :- calificacion(E, C, N, P), R is N * P.
 
-%notas(E, C) :- findall(R , nota_ponderada(E, C, R),L).
-%suma([],0).
-%suma([N|Y],M):- suma(Y, M1), M is M1 + N.
-%nota_final(E, C, N) :- findall(R, nota_ponderada(E, C, R), N).
+% Lista de calificaciones
+lista_notas(E, C, L) :- bagof(N, nota_ponderada(E, C, N), L).
 
-%suma([],0).
-%suma([N|Y],M):- suma(Y, M1), M is M1 + N.
-
-%resultado(E, C, N) :- findall( Y, calificacion(E,C,N,P), Resultado).
-%resultado(E, C, L) :- findall(R, nota_ponderada(E, C, R), L).
+% Nota final por estudiante
+nota_final(E, C, Nf) :- lista_notas(E, C, L), suma(L, S), Nf is S.
